569168cd4f56b4dcb6c330825e40d8f889e8522c
sibteali786 <sibteali786@gmail.com>
2022-04-10 17:11:06 +0500

    done with desktop footer

--- src/mainLayout/Footer/Footer.js
+++ src/mainLayout/Footer/Footer.js
@@ -29,11 +29,142 @@ const Footer = () => {
                     </IconButton>
                 </div>
             </div>
-            <div className="main-footer">   
+            <div className="main-footer">
+                <div className="linksSection">
+                    
+                <div className='main-elements'>
+                    <h3>
+                    Servicio al Cliente:
+                    </h3>
+                    <div>
 
-            </div>
+
+                    <a>
+                    Como comprar
+
+                    </a>
+                    <a>
+                    Seguimiento de pedido
+
+                    </a>
+                    <a>
+                    Whatsapp
+
+                    </a>
+                    <a>
+                    Llámanos
+
+                    </a>
+                    <a>
+                    Contacto
+
+                    </a>
+                    <a>
+
+                    Comentarios y 
+                    </a>
+                    <a>
+                    Sugerencias
+                    </a>
+                    <a>
+                    Referidos
+
+                    </a>
+                    </div>
+                </div>
+                <div className='main-elements'>
+                    <h3>Florería Suecia</h3>
+                    <div>
+
+                    <a>
+                    Sobre nosotras
+
+                    </a>
+                    <a>
+                    Nuestra diferencia
+
+                    </a>
+                    <a>
+                    Nuestra ubicación
+
+                    </a>
+                    <a>
+                    Políticas de devoluciones
+
+                    </a>
+                    <a>
+                    Términos y condiciones
+
+                    </a>
+                    <a>
+
+                    Trabaja con nosotras
+                    </a>
+                    <a>
+                    Afiliados
+
+                    </a>
+                    </div>
+                </div>
+                <div className='main-elements'>
+                    <h3>Pedidos</h3>
+                    <div>
+
+                    <a>
+                        Preguntas frecuentes
+                    </a>
+                    <a>
+                        Cambiar datos de pedido
+
+                    </a>
+                    <a>
+                        Reclamo
+                    </a>
+                    <a>
+                        
+                    Devolución
+                    </a>
+                    </div>
+                </div>
+                <div className='main-elements'>
+                    <div >
+                        <h3>Ocasión:</h3>
+                        <div>
+                        <div>
+
+                        <a>
+                            Cumpleaños
+                        </a>
+                        <a>
+                        Aniversario
+
+                        </a>
+                        <a>
+
+                        Condolencias
+                        </a>
+                        <a>
+                        Agradecimiento
+
+                        </a>
+                        <a>
+                            
+                        Porque sí
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                    <div style={{marginTop:"2rem"}} >
+                        <h3>Mi Cuenta</h3>
+                        <a>Acceder a mi cuenta</a>
+                    </div>
+                </div>
+            </div>   
             <div className="copyrights">
-
+            2022 1-800 flowers.com, inc, jericho, NY family of brands
+            <a>terms of use</a>
+            <a>Privacy notice</a>
+            </div>
             </div>
             
         </div>        
