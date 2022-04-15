ca0ddd946e8499180b61b32cb9a1b35e307c6178
sibteali786 <sibteali786@gmail.com>
2022-04-16 02:41:54 +0500

    added top navigation in products page

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -7,11 +7,49 @@ import 'swiper/swiper.scss'; // core Swiper
 import 'swiper/modules/navigation/navigation.scss'; // Navigation module
 import 'swiper/modules/thumbs/thumbs.scss';
 import "./Products.scss";
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Stack from '@mui/material/Stack';
 
 const Products = () => {
+    function handleClick(event) {
+        event.preventDefault();
+        console.info('You clicked a breadcrumb.');
+      }
     const [activeThumb, setactiveThumb] = useState();
+    const breadcrumbs = [
+        <a  key="1" href="/" onClick={handleClick}>
+          Estado de México
+        </a>,
+        <a
+          key="2"
+          href="/material-ui/getting-started/installation/"
+          onClick={handleClick}
+        >
+          Atizapan de Zaragoza
+        </a>,
+        <a key="3" style={{color:"#72509D",fontWeight:"bold"}}>
+          Vino 3V con Botanas
+        </a>,
+      ];
   return (
     <div className="ProductContainer">
+        <div className="topNavigation">
+            <Stack spacing={2}>
+                <Breadcrumbs separator="›" aria-label="breadcrumb">
+                    {breadcrumbs}  
+                </Breadcrumbs>
+            </Stack>
+            <Stack spacing={2}>
+                <Breadcrumbs separator="|" aria-label="breadcrumb">
+                    <a>
+                    Enviar a
+                    </a>  
+                    <a style={{fontWeight:"bold"}}>
+                    Benito Juarez
+                    </a>
+                </Breadcrumbs>
+            </Stack>
+        </div>
         {/* Product Display Slider */}
         <div className="sliderProduct">
         <Swiper 
