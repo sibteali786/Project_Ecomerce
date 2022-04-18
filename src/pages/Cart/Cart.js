b7671d078d7b48d2bdd258a3fea2a75360a77998
sibteali786 <sibteali786@gmail.com>
2022-04-19 04:40:34 +0500

    made some changes in divider

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -320,6 +320,7 @@ const Cart = () => {
                 display: "grid",
                 gridTemplateColumns: "0.45fr 0.1fr 0.45fr",
                 gap: "1rem",
+                marginBottom:"1rem"
               }}
             >
               <div
