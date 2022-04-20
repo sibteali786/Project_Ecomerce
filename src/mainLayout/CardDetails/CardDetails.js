3618bad7398dce4b7eb6abfe2a98c3888f2a81a6
sibteali786 <sibteali786@gmail.com>
2022-04-20 16:22:41 +0500

    done with step 4

--- src/mainLayout/CardDetails/CardDetails.js
+++ src/mainLayout/CardDetails/CardDetails.js
@@ -6,19 +6,24 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import "./CardDetails.scss";
 
+
+
+
+
 const CardDetails = () => {
   return (
     <div className="Step4">
       <div className="span-1">
         <div>
           <h3>Secured Checkout</h3>
+          <div style={{display: "flex",justifyContent: "flex-start"}}>
           <p>Already Registered ?</p>
           <a>Click here to login</a>
+          </div>
         </div>
       </div>
       <div className="span-2">
         <h6>Billing Address</h6>
-        <div>
           <div>
             <label htmlFor="html"> FirstName* </label>
             <TextField
@@ -35,7 +40,6 @@ const CardDetails = () => {
               InputLabelProps={{ shrink: false }}
             />
           </div>
-        </div>
         <div>
           <label htmlFor="html"> Email* </label>
           <TextField
@@ -52,7 +56,7 @@ const CardDetails = () => {
             InputLabelProps={{ shrink: false }}
           />
         </div>
-        <div>
+        <div style={{gridColumn:'1/3'}}>
           <label htmlFor="html"> Address* </label>
           <TextField
             label=""
@@ -107,7 +111,7 @@ const CardDetails = () => {
             />
           </FormGroup>
         </div>
-        <Button variant="contained" className="buttonViolet">
+        <Button variant="contained" style={{gridColumn:'1/3'}} className="buttonViolet">
           Finalizar compra
         </Button>
       </div>
@@ -168,15 +172,74 @@ const CardDetails = () => {
         </div>
       </div>
       <div className="span-5">
-        <h6>
-        Revisa lo que llevas
-        </h6>
-        <p style={{color:"#818181",fontSize:"0.8rem"}}>
-        Productos envaidos pos Drafiti
+        <h6>Revisa lo que llevas</h6>
+        <p style={{ color: "#818181", fontSize: "0.8rem" }}>
+          Productos envaidos pos Drafiti
         </p>
-        <div>
-            
+        <div className="reviewProductCard">
+          <img
+            src={require("./../../assets/Rectangle69.png").default}
+            alt="productImage"
+          />
+          <div>
+            <h6>DEMIN</h6>
+            <p className="productdDetails">
+              Method de page Todos ios methdos habiito.
+            </p>
+            <p>TALE : 1</p>
+            <p>cantidad : 1 Combiar</p>
+          </div>
+          <strong>$9.900</strong>
         </div>
+        <div className="productPriceTotal">
+            <div>
+              <p>Sub Total</p>
+              <p>$9.900</p>
+            </div>
+            <div>
+              <p>Shipping</p>
+              <p>$9.9</p>
+            </div>
+            <div>
+              <strong>Other Total</strong>
+              <p>$9.9</p>
+            </div>
+          </div>
+          <div style={{marginLeft: "auto",gridColumn: "1/5"}}>
+            <label htmlFor="html"> Usar cupón </label>
+            <TextField
+              label=""
+              variant="outlined"
+              style={{margin:"0 1rem"}}
+            />
+            <Button variant="contained" className="buttonViolet">
+              Apply
+            </Button>
+          </div>
+              <p style={{gridColumn:"1/3"}}>
+              Si quieres puedes escribir un mensaje
+aquí y lo enviaremos en un lindo sobre
+              </p>
+              <TextField
+                label=""
+                variant="outlined"
+                multiline
+                maxRows={3}
+                style={{gridColumn:"3/5"}}
+              />
+          <FormGroup style={{gridColumn: "3/5"}}>
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Demuestra tu agradec "
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Demuestra tu agradec "
+            />
+          </FormGroup>
+          <Button fullWidth variant="contained" className="buttonViolet">
+          Finalizar compra
+          </Button>
       </div>
     </div>
   );
